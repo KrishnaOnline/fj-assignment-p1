95a23eaa9393be09c6b6a54b1ce2d1e9fbd7248e
KrishnaOnline <krishnavamshikusuma@gmail.com>
2024-09-10 18:35:45 +0530

    loader added on home page

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -6,11 +6,14 @@ import { Link } from "react-router-dom";
 
 function Home() {
     const [movies, setMovies] = useState([]);
+    const [loading, setLoading] = useState(false);
     const {token} = useSelector(state => state.auth);
     console.log(token);
     const fetchAllMovies = async () => {
+        setLoading(true);
         const response = await getAllMovies();
         setMovies(response);
+        setLoading(false);
     }
     useEffect(() => {
         fetchAllMovies();
@@ -24,31 +27,39 @@ function Home() {
                         Create new Movie
                     </Link>
                     <div className="flex items-center justify-center gap-3">
-                        <Link className="text-lg border-2 border-gray-700 p-[5px] px-[10px] rounded" to={"/actors"}>All Actors</Link>
-                        <Link className="text-lg border-2 border-gray-700 p-[5px] px-[10px] rounded" to={"/producers"}>All Producers</Link>
+                        <Link className="text-lg border-2 border-gray-700 p-[5px] px-[10px] rounded bg-app text-black" to={"/actors"}>All Actors</Link>
+                        <Link className="text-lg border-2 border-gray-700 p-[5px] px-[10px] rounded bg-app text-black" to={"/producers"}>All Producers</Link>
                     </div>
                 </div>
                 <div className="flex items-center px-10 gap-2">
-                    <p className="text-lg underline pr-3">Profiles: </p>
+                    {/* <p className="text-lg underline pr-3">Profiles: </p> */}
                     <Link className="text-lg border-2 border-gray-700 bg-[#1E3A8A] p-[5px] px-[10px] rounded" target="_blank" to={"https://krishnavamshi-portfolio.netlify.app/"}>Portfolio</Link>
                     <Link className="text-lg border-2 border-gray-700 bg-[#242938] p-[5px] px-[10px] rounded" target="_blank" to={"https://github.com/KrishnaOnline"}>GitHub</Link>
                     <Link className="text-lg border-2 border-gray-700 bg-[#0077B5] p-[5px] px-[10px] rounded" target="_blank" to={"https://www.linkedin.com/in/krishna-vamshi-kusuma-11717b213/"}>LinkedIn</Link>
                 </div>
             </div>
-            <div className="flex mt-10 flex-wrap justify-center /*justify-center*/ gap-5">
-                {
-                    movies?.length===0
-                    ?
-                    <div className="flex px-10 gap-1 text-lg">
-                        <p>No Movies to display, </p>
-                        <Link className="underline text-blue-500" to={"/add-movie"}>Create One?</Link>
-                    </div>
-                    :
-                    movies?.map(m => (
-                        <MovieCard key={m._id} movie={m}/>
-                    ))
-                }
-            </div>
+            {
+                loading===true
+                ?
+                <div className="flex mt-40 justify-center text-5xl">
+                    <p>Loading...</p>
+                </div>
+                :
+                <div className="flex mt-10 flex-wrap justify-center /*justify-center*/ gap-5">
+                    {
+                        movies?.length===0
+                        ?
+                        <div className="flex px-10 gap-1 text-lg">
+                            <p>No Movies to display, </p>
+                            <Link className="underline text-blue-500" to={"/add-movie"}>Create One?</Link>
+                        </div>
+                        :
+                        movies?.map(m => (
+                            <MovieCard key={m._id} movie={m}/>
+                        ))
+                    }
+                </div>
+            }
         </div>
     );
 }
